bcb5b2565378d3f2a0ea83bb82fdf24d084d858e
Lina <lina.buineviciute@gmail.com>
2018-02-23 19:05:48 +0200

    sukelius visus projekto dokus

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#treciaEil').click(function() {
+    $('#trecias1').remove();
+    $('#trecias').removeClass("col s6");
+    $('#trecias').addClass("col s12");
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
